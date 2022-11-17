add59b00cda0ec27208601374cd1d16c0647d143
handnew <handnew04@gmail.com>
2022-11-17 19:40:05 +0900

    init commit

---
+++ InstargramClone/InstargramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstargramCloneApp.swift
+//  InstargramClone
+//
+//  Created by yujung on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct InstargramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
